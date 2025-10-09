1c965a7c75b74c59b6232169bc2c628cfa617dce
ottohartel <otto.haertel@gmail.com>
2025-10-09 23:52:52 +0200

    feat: settng random jitter up to 1 sec

--- FuelDistanceCalculator/Services/MarketFuelPriceService.cs
+++ FuelDistanceCalculator/Services/MarketFuelPriceService.cs
@@ -25,7 +25,7 @@ public class MarketFuelPriceService
     try
     {
         if(DateTime.Now.Second==0){
-            await Task.Delay(new Random().Next(400, 600));
+            await Task.Delay(new Random().Next(400, 1000));
         }
         var response = await _httpClient.GetAsync(requestUrl);
 

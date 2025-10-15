84b3c25a85ba6a07f0ac149bc55ac07819b88101
ottohartel <otto.haertel@gmail.com>
2025-10-15 09:05:04 +0200

    feat: introducing second sorting

--- FuelDistanceCalculator/Services/TankCostService.cs
+++ FuelDistanceCalculator/Services/TankCostService.cs
@@ -8,9 +8,10 @@ public static class TankCostService{
         if (fuelAmount <= 0)
         {
             return stations.Where(station => station.IsOpen)
-                        .OrderBy(station => station.FuelTypePrice)
-                        .Take(10)
-                        .ToList();
+               .OrderBy(station => station.FuelTypePrice)  // Primär: Aufsteigend nach Preis
+               .ThenBy(station => station.Dist)            // Sekundär: Bei gleichem Preis aufsteigend nach Entfernung
+               .Take(10)
+               .ToList();
         }
             Console.WriteLine("Parallel working started");
             var stationCosts = stations

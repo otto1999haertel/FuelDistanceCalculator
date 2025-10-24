2d078cad6f9fd8cc5a4477dbf913b8825d510f9d
ottohartel <otto.haertel@gmail.com>
2025-10-24 20:13:22 +0200

    chore: remove unused code

--- FuelDistanceCalculator/Model/ResponseModelMarketFuelPriceService.cs
+++
@@ -1,7 +0,0 @@
-namespace FuelDistanceCalculator.Model;
-public class ResponseModelMarketFuelPriceService
-{
-    public string status { get; set; } = string.Empty;
-    public bool ok { get; set; }
-    public string message { get; set; } = string.Empty;
-}

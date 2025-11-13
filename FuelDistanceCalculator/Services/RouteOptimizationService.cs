529880ee4fcc36a548e4ddbb40357aa3501d1e81
ottohartel <otto.haertel@gmail.com>
2025-11-13 09:45:00 +0100

    chore: create road map

--- FuelDistanceCalculator/Services/RouteOptimizationService.cs
+++ FuelDistanceCalculator/Services/RouteOptimizationService.cs
@@ -4,6 +4,9 @@ public class RouteOptimizationService : IRouteOptimizationService
 {
     public Task<GasStation> FindBestStationAsync(List<CoordinatesDTO> searchPointsOnRoute, FuelType fuelType, double litersToFill, double maxDistanceKm, double intervalKm)
     {
+        //Foreach searpoint search for Gasstaions in maxDistance
+        //Check Gasstations if in forward cone
+        //Calculate total cost for each found GasStation
         throw new NotImplementedException();
     }
 }

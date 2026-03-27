b8384917fc86c09b55fc6ed6208f4447ee1ee04f
ottohartel <otto.haertel@gmail.com>
2026-03-27 12:37:16 +0100

    feat:extarcting infos from pdf works rudemantlly

--- Scripts/createJSON.py
+++ Scripts/createJSON.py
@@ -6,6 +6,7 @@ import camelot
 from pathlib import Path
 
 Input_PDF = "autokostenuebersicht.pdf"
+Output_JSON = "ADAC_car_data.json"
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 output_json_dir = os.path.join(current_dir, "..", "FuelDistanceCalculator", "Data")
@@ -22,6 +23,15 @@ if not os.path.exists(output_json_dir):
     raise(f"Output dir {output_json_dir} not found")
 
 camelot_tables = camelot.read_pdf(file_path, pages="3-end", flavor="stream")
-
 for table in camelot_tables:
+    #Row index 4 is brand
+    #First Column is model
+    #Second Column is power
+    #Last Column is cost per kilometer
+    current_brand= table.df.iloc[4,0]
+    for row in table.data[5:]:
+        model = row[0]
+        power = row[1]
+        cost_per_km = row[-1]
+        print(f"Brand: {current_brand}, Model: {model}, Power: {power}, Cost per km: {cost_per_km}")
     print(table.df)

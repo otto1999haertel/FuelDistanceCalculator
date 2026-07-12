42d4f3c92df70a7ae8981b7836a2edae91e1a99e
ottohartel <otto.haertel@gmail.com>
2026-07-12 13:18:24 +0200

    test: compare location cache test

--- FuelDistanceCalculatorE2ETest/CacheTest.cs
+++ FuelDistanceCalculatorE2ETest/CacheTest.cs
@@ -67,7 +67,12 @@ public class CacheTest : E2EBaseTest
     }
 
     [Test]
-    public async Task SettingUpCompareLoctionsShouldCachePLZandRadius_Test()
+    [TestCase("01067", true)]
+    [TestCase("Dresden", false)]
+    [TestCase("01067 Dresden", false)]
+    [TestCase("123456", false)]
+    [TestCase("Max-Mustermann-Str. 1, 01067 Dresden", false)]
+    public async Task SettingUpCompareLoctionsShouldCachePLZandButNotConcretePlace_Test(string places, bool isCached)
     {
         await Page.GotoAsync("/");
 
@@ -75,17 +80,42 @@ public class CacheTest : E2EBaseTest
 
         var standort1Input = Page.Locator("#FirstComparePlace");
 
-        var standort1Radius = Page.Locator("#RadiusPlace0");
-
-        await standort1Input.FillAsync("01067");
-
-        await standort1Radius.FillAsync("15");
+        await standort1Input.FillAsync(places);
 
         var standort2Input = Page.Locator("#SecondComparePlace");
 
-        var standort2Radius = Page.Locator("#RadiusPlace1");
+        await standort2Input.FillAsync(places);
+        await standort2Input.PressAsync("Tab");  
 
-        await standort2Input.FillAsync("01069");
+        await Page.ReloadAsync();
+
+        ClickCompareLocationsTab();
+
+        if (isCached)
+        {
+            await Expect(standort1Input).ToHaveValueAsync(places);
+            await Expect(standort2Input).ToHaveValueAsync(places);
+        }
+        else
+        {
+            await Expect(standort1Input).Not.ToHaveValueAsync(places);
+            await Expect(standort2Input).Not.ToHaveValueAsync(places);
+        }
+
+
+    }
+
+    [Test]
+    public async Task SettingUpCompareLoctionsShouldCacheRadiusAfterReload_Test()
+    {
+        await Page.GotoAsync("/");
+
+        ClickCompareLocationsTab();
+
+        var standort1Radius = Page.Locator("#RadiusPlace0");
+        await standort1Radius.FillAsync("15");
+
+        var standort2Radius = Page.Locator("#RadiusPlace1");
 
         await standort2Radius.FillAsync("20");
 
@@ -93,13 +123,38 @@ public class CacheTest : E2EBaseTest
 
         ClickCompareLocationsTab();
 
-        await Expect(standort1Input).ToHaveValueAsync("01067");
         await Expect(standort1Radius).ToHaveValueAsync("15");
-        await Expect(standort2Input).ToHaveValueAsync("01069");
         await Expect(standort2Radius).ToHaveValueAsync("20");
 
     }
 
+    [Test]
+    public async Task SettingUpCompareLoctionsShouldCacheDifferentPlaces_Test()
+    {
+        await Page.GotoAsync("/");
+
+        ClickCompareLocationsTab();
+
+        var standort1Input = Page.Locator("#FirstComparePlace");
+
+        await standort1Input.FillAsync("01067");
+
+
+        var standort2Input = Page.Locator("#SecondComparePlace");
+
+
+        await standort2Input.FillAsync("01069");
+        await standort2Input.PressAsync("Tab");  
+
+        await Page.ReloadAsync();
+
+        ClickCompareLocationsTab();
+
+        await Expect(standort1Input).ToHaveValueAsync("01067");
+        await Expect(standort2Input).ToHaveValueAsync("01069");
+
+    }
+    
     private async Task ClickCompareLocationsTab()
     {
         var manualTab = Page.Locator("label[for='mode_man']")

93b4b6cdaad6ec596a1dcd784f68a2d7c5ccfe25
Haertel <otto.haertel@siemens.com>
2026-07-14 10:37:53 +0200

    test: adding optimal search with fuel amount test

--- FuelDistanceCalculatorE2ETest/OptimalSearchTest.cs
+++ FuelDistanceCalculatorE2ETest/OptimalSearchTest.cs
@@ -30,8 +30,41 @@ public class OptimalSearchTest : E2EBaseTest
     }
 
     [Test]
-    public async Task OptimalSearch_With_Max_FuelAmount_Displays_PricePerLiter()
+    public async Task OptimalSearch_With_Max_FuelAmount_Displays_GesamtKosten_And_Ersparnis()
     {
-        Assert.Fail("Test expüected to fail to test pipleine");
+        await Page.GotoAsync("/");
+
+        // 3. Start- und Zielort setzen
+        await Page.Locator("#generalLocationInput").FillAsync("Dresden");
+
+        // 1. Maximale Tankmenge setzen
+        await Page.Locator("#MaximumFuelAmount").FillAsync("60");
+
+        // 2. Fokus verlassen, damit das 'blur'-Event greift
+        await Page.Locator("#MaximumFuelAmount").PressAsync("Tab");
+
+        await Page.Locator("#FuelAmountRange").EvaluateAsync(
+            "el => { el.value = '30'; el.dispatchEvent(new Event('input', { bubbles: true })); }"
+        );
+
+        // 4. Berechnung starten SearchBtn
+        await Page.Locator("#SearchBtn").ClickAsync();
+
+
+        string savingsText = await Page.Locator("#savings-to-cheapest-station").InnerTextAsync();
+        Assert.That(savingsText, Does.StartWith("Ersparnis zum günstigsten Einzelpreis im Radius:"), $"Erwarteter Text nicht gefunden. Tatsächlicher Text: '{savingsText}'");
+
+        string nearestSavingsText = await Page.Locator("#savings-to-nearest-station").InnerTextAsync();
+        Assert.That(nearestSavingsText, Does.StartWith("Ersparnis zur Nächsten im Radius:"), $"Erwarteter Text nicht gefunden. Tatsächlicher Text: '{nearestSavingsText}'");
+
+        // 5. Warten auf die Anzeige der Ergebnisse
+        string regexPattern = @".* Gesamtkosten .*: [0-9]+,[0-9]+ €";
+
+        var firstSummary = Page.Locator("#station-summary-item-0");
+        string summaryText = await firstSummary.InnerTextAsync();
+        summaryText = summaryText.Replace("\n", " ").Trim();
+
+        Assert.That(summaryText, Does.Match(regexPattern),
+            $"Erwarteter Text nicht gefunden. Tatsächlicher Text: '{summaryText}'");
     }
 }
